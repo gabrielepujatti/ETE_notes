3ea491b23fb9d48608d2bdfb215725769692093b
gabrielepujatti <151385780+gabrielepujatti@users.noreply.github.com>
2024-08-18 16:57:11 +0200

    miscellaneous setup changes

--- filters/proof-callout.lua
+++ filters/proof-callout.lua
@@ -1,15 +1,10 @@
-
-local h2 = pandoc.Header(2, "Proof")
-
 function Div(el)
-    if quarto.doc.isFormat("html") then
-        if el.classes:includes('proof') then
-            local content = el.content
-            table.insert(content, 1, h2)
-            return pandoc.DIv(
-                content,
-                {class="callout-tip", collapse="true", style="minimal"}
-            )
-        end
+  if quarto.doc.isFormat("html") then
+    if el.classes:includes('proof-box') then
+
+      el.classes = {"callout", "callout-tip"}
+      el.attributes["collapse"] = "true"
+      el.attributes["appearance"] = "minimal"
     end
-end
+  end
+end 
